88a8031300c515189d913135968e9e2c2e9f3b9b
Glenn de Smidt <glenn@destar.co.uk>
2019-02-08 15:53:49 +0000

    slight tangent to add and test decoding of data using parse_socketIO_packet_data - will restructure now that it's working

--- bin/request.py
+++ bin/request.py
@@ -4,52 +4,37 @@
 
 Usage:
         request.py --help
-        request.py 
+        request.py [--host=host] [--port=port]
 
 
 Options:
-        --start=start-date                              specify a start date for billing YYYY-MM-DD
-        --end=end-date                                  specify an end date for billing YYYY-MM-DD
-        --last-month                                    standard billing mode, i.e. bill for last month
-        --email=recipient                               override the default destination of accounts@destar.co.uk
-        --smpt-relay=<relay>                            set the SMTP relay to use [default: email01].
-        --xero                                          use Xero API
+        --host=host                                     Host [default: localhost]
+        --port=port                                     Port number [default: 8080]
 
 """
 import docopt
 import logging
 import coloredlogs
 import socketIO_client
-import base64
+import pprint
+import sys
 
 import json
 import socket
 
 logger = logging.getLogger( __name__ )
+opts = docopt.docopt( __doc__, version='GWSS Client v1.0' )
+ws = socketIO_client.SocketIO( host=opts.get( '--host' ), port=int( opts.get( '--port' ) ) )
 
-def on_connect( ):
-    logger.debug( "on_connect" )
-
-def on_error(  ):
-    logger.debug( "on_error:", error )
-
-def on_disconnect(  ):
-    logger.debug( "on_disconnect" )
-    
-def on_reconnect(  ):
-    logger.debug( "on_reconnect" )
-
-def on_response( args ):
-    print( 'on_response (gwss_response)', args )
+        
+def on_response( data ):
+    d = socketIO_client.parsers.parse_socketIO_packet_data( data )
+    logger.debug( 'on_response ({}): {}'.format( d.args[0], d.args[1] ) )
 
 if __name__ == '__main__':
-    opts = docopt.docopt( __doc__, version='GWSS Client v1.0' )
     delay = 2
     
     logger.setLevel( logging.DEBUG )
-    # handler = logging.handlers.SysLogHandler( address = '/dev/log', facility=logging.handlers.SysLogHandler.LOG_USER )
-    # handler.setLevel( logging.DEBUG )
-    # logger.addHandler( handler )
     coloredlogs.install( level='DEBUG' )
     logger.info( "running websocket client" )    
 
@@ -61,23 +46,27 @@ if __name__ == '__main__':
                     "item3": "bobble",
                     "item4": -1,
                 },
-
     }
     
-    ws = socketIO_client.SocketIO( 'localhost', 8080, socketIO_client.LoggingNamespace )
     logger.debug( "assigning event triggers" )
-    ws.on( 'connect', on_connect )
-    ws.on( 'error', on_error )
-    ws.on( 'disconnect', on_disconnect )
-    ws.on( 'reconnect', on_reconnect )
-    ws.on( 'gwss_response', on_response )
+    # ws.on( 'connect', on_connect )
+    # ws.on( 'error', on_error )
+    # ws.on( 'disconnect', on_disconnect )
+    # ws.on( 'reconnect', on_reconnect )
+    # ws.on( 'slinky', on_response )
+    ws.on( 'message', on_response )
+
     logger.debug( "sending gwss_request" )
     ws.emit( 'gwss_request', command )
-    
     ws.wait( seconds = 1 )
-    command.update( { "request": "stuff.slinky" } )
-    command['data'].update( { 'peddles': 100 } )
-    ws.emit( 'gwss_request', command )
+
+    c = 100
+    while c < 110:
+        command.update( { "request": "stuff.slinky" } )
+        command['data'].update( { 'peddles': c } )
+        ws.emit( 'gwss_request', command )
+        c += 1
+        
     logger.info( "waiting {} seconds to exit".format( delay ) )
     ws.wait( seconds = delay )
             
